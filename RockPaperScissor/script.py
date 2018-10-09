db52dc5b88cb408166e855800f77c7b7095f91d0
ayushianan <ayushi2712@gmail.com>
2018-10-09 21:49:04 +0530

    Adding more options and reducing time

--- RockPaperScissor/script.py
+++ RockPaperScissor/script.py
@@ -1,37 +1,47 @@
 from random import randint
 
-#Input from player
-player = input('rock (r), paper (p) or scissor (s)\n')
-
-print(player, 'vs ', end='')
-
-#computers turn111
-chosen = randint(1,3)
-
-if chosen == 1:
-	computer = 'r'
-	print('0')
-
-elif chosen == 2:
-	computer = 'p'
-	print('__')
-elif chosen == 3:
-	computer = 's'
-	print('>8')
+t='y'
 
 #result
+while t=='y':
+    player = input('rock (r), paper (p) or scissor (s)\n')
 
-if player == computer:
-	print('Draw!')
-elif player == 'r' and computer == 'p':
-	print('Computer wins')
-elif player == 'r' and computer == 's':
-	print('Player wins')
-elif player == 'p' and computer == 'r':
-	print('Player wins')
-elif player == 'p' and computer == 's':
-	print('Computer wins')
-elif player == 's' and computer == 'p':
-	print('Player wins')
-elif player == 's' and computer == 'r':
-	print('Computer wins')
+    print(player, 'vs ', end='')
+
+    # computers turn111
+    chosen = randint(1, 3)
+
+    if chosen == 1:
+        computer = 'r'
+        print('0')
+
+    elif chosen == 2:
+        computer = 'p'
+        print('__')
+    elif chosen == 3:
+        computer = 's'
+        print('>8')
+
+    if player == computer:
+	    print('Draw!')
+    elif player == "r":
+        if computer == "p":
+            print("You lose")
+        else:
+            print("You win")
+    elif player == "p":
+        if computer == "s":
+            print("You lose")
+        else:
+            print("You win")
+    elif player == "s":
+        if computer == "r":
+            print("You lose")
+        else:
+            print("You win")
+    else:
+        print("Not a valid choice")
+        # player was set to True, but we want it to be False so the loop continues
+    print("want to play more ")
+    t=input('y/n')
+
